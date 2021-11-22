80fb31b5296dd22c898d0e7098d867ec274748ee
javillao <javillao@espol.edu.ec>
2021-11-22 11:13:00 -0500

    Migrations files

---
+++ database/migrations/2021_11_22_012628_create_invoice_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('identification_id')->constrained('identification');
+            $table->date('date');
+            $table->float('total', 10, 2);
+            $table->string('observation');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice');
+    }
+}
